bf367305cd0004ffd4dd4fb574132e985134d6eb
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-09-30 12:51:45 +0200

    Initial commit

---
+++ src/main/java/cristianorocchi/graffitiplatform/GraffitiplatformApplication.java
@@ -0,0 +1,13 @@
+package cristianorocchi.graffitiplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraffitiplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraffitiplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cristianorocchi/graffitiplatform/GraffitiplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package cristianorocchi.graffitiplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraffitiplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
